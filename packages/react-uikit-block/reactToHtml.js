07f1a77e64a538088ec4342137d796506307448c
Murray Lucas <murray.lucas@orionhealth.com>
2017-05-13 08:17:39 +1200

    SQUASH: Playing with html to markdown

--- packages/react-uikit-block/reactToHtml.js
+++ packages/react-uikit-block/reactToHtml.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import path from 'path';
+import toMarkdown from 'to-markdown';
 
 
 const args = process.argv.slice(2);
@@ -9,4 +10,4 @@ const Component = require(path.resolve(__dirname, args[0]));
 
 const htmString = ReactDOMServer.renderToStaticMarkup(<Component.default />);
 
-process.stdout.write(htmString);
+process.stdout.write(toMarkdown(htmString));
