fe129acd3e180cf3948f4d46d2342a0f5b12a744
Murray Lucas <murray.lucas@orionhealth.com>
2017-05-13 15:02:30 +1200

    Moved common helpers dir to a package so that the path references will be consistent between the website and the packages.

---
+++ packages/common-helpers/components/DocHeader.jsx
@@ -0,0 +1,39 @@
+import React, { PropTypes } from 'react';
+import Codeblock from 'react-uikit-codeblock';
+
+const DocHeader = ({ name, npmName, summary }) => (
+  <div>
+    <section>
+      <h1>React Uikit { name }</h1>
+      <p className='uk-article-lead'>{ summary }</p>
+      <p>
+        See <a href="http://otissv.github.io/react-uikit-components">the website </a> for docs.
+      </p>
+    </section>
+    <section>
+      <h2>Usage</h2>
+      <Codeblock lang="bash">
+{`
+npm install ${npmName} --save;
+`}
+      </Codeblock>
+      <Codeblock lang="javascript">
+{`
+// ES6
+import ${name} from '${npmName}';
+
+// ES5
+var ${name} = require('${npmName}');
+`}
+      </Codeblock>
+    </section>
+  </div>
+);
+
+DocHeader.propTypes = {
+  name: PropTypes.string.isRequired,
+  npmName: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+};
+
+export default DocHeader;
