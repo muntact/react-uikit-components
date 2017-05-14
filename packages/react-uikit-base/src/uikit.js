a371454cc3cfd148670c38a048371f5715aa641b
Murray Lucas <murray.lucas@orionhealth.com>
2017-05-14 16:25:39 +1200

    FIX: optimised the way that react-uikit-base exports functions?

--- packages/react-uikit-base/src/uikit.js
+++ packages/react-uikit-base/src/uikit.js
@@ -1,21 +1,23 @@
 'use strict';
 
-import Helpers from './helpers';
-import Element from './element';
-import Base from './base';
-import Events from './events';
-import Utility from './utility';
+import helpers from './helpers';
+import element from './element';
+import base from './base';
+import events from './events';
+import utility from './utility';
 
-export const helpers = Helpers;
-export const element = Element;
-export const base = Base;
-export const events = Events;
-export const utility = Utility;
+export {
+  helpers,
+  element,
+  base,
+  events,
+  utility
+};
 
 export default {
-  helpers: Helpers,
-  element: Element,
-  base: Base,
-  events: Events,
-  utility: Utility
+  helpers,
+  element,
+  base,
+  events,
+  utility
 };
