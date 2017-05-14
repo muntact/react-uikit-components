8e0b1790d2bc123cb03363e4c4447f27f330b4bc
Murray Lucas <murray.lucas@orionhealth.com>
2017-05-14 16:29:16 +1200

    optimised the react-uikit-block documentation

--- packages/react-uikit-block/docs/block-doc.jsx
+++ packages/react-uikit-block/docs/block-doc.jsx
@@ -1,415 +1,155 @@
-'use strict';
+import React, { PropTypes } from 'react';
 
-import React from 'react';
-import Codeblock from 'react-uikit-codeblock';
-import Note from 'react-uikit-note';
-import Table from 'react-uikit-table';
-import Block from 'react-uikit-block';
+import Block from '../lib/block';
 import Grid from 'react-uikit-grid';
+import Note from 'react-uikit-note';
 import Panel from 'react-uikit-panel';
+import Table from 'react-uikit-table';
 
+import DocHeader from '../../common-helpers/lib/DocHeader';
+import DocExample from '../../common-helpers/lib/DocExample';
+import DocFooter from '../../common-helpers/lib/DocFooter';
 
-const BlockDoc = (props) => (
-  <div>
-    <section>
-      <h1>Block</h1>
-      <p>
-        Separate content sections by bundling them in blocks.
-      </p>
-      <p>
-        <a href='https://github.com/otissv/react-uikit-block'>react-uikit-block</a> on github.
-      </p>
-    </section>
+const name = 'Block';
+const npmName = 'react-uikit-block';
+const summary = 'Separate content sections by bundling them in blocks with different styles.';
+const propTuples = [
+  {
+    Prop: "context",
+    Type: "oneOf: 'default' | 'muted' | 'primary' | 'secondary'"
+  },
+  {
+    Prop: "contrast",
+    Type: "bool"
+  },
+  {
+    Prop: "large",
+    Type: "bool"
+  },
+];
 
-    <section>
-      <h2>Usage</h2>
-      <Codeblock>
-{`npm install react-uikit-block --save;
+const generateExampleJSX = ({ context, container = false, contrast, key, large, title = 'block' }) => (
+  <Block {...{ context, container, contrast, large, key }}>
+    <h4>{title}</h4>
+    <Grid widths='1-3'>
+      <Panel>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna.
+      </Panel>
+      <Panel>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna.
+      </Panel>
+      <Panel>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna.
+      </Panel>
+    </Grid>
+  </Block>
+);
 
-// ES6
-import Block from 'react-uikit-block';
+generateExampleJSX.propTypes = {
+  context: PropTypes.string.isRequired,
+  container: PropTypes.bool.isRequired,
+  contrast: PropTypes.bool.isRequired,
+  key: PropTypes.number,
+  large: PropTypes.bool.isRequired,
+  title: PropTypes.string.isRequired,
+};
 
-// ES5
-var Block = require('react-uikit-block').default;`}
-      </Codeblock>
+const generateCodeSnippet = ({ context, container, contrast, large, title }) =>
+`<Block ${context ? `context='${context} '` : null }${container ? "container " : null }${contrast ? "contrast " : null }${large ? "large " : null }>
+  <h4>${title}</h4>
+  <Grid widths='1-3'>
+    <Panel>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna.
+    </Panel>
+    <Panel >
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna.
+    </Panel>
+    <Panel >
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna.
+    </Panel>
+  </Grid>
+</Block>`;
 
-      <hr className="uk-article-divider" />
+const mutedArgs = { context: "muted" };
+const mutedExample = generateExampleJSX(mutedArgs);
+const mutedSnippet = generateCodeSnippet(mutedArgs);
 
-      <h3 className='example'>Example</h3>
-      <Block context='muted'>
-        <h4>Block</h4>
-        <Grid widths='1-3'>
-          <Panel>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna.
-          </Panel>
-          <Panel >
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna.
-          </Panel>
-          <Panel >
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna.
-          </Panel>
-        </Grid>
-      </Block>
+const containerArgs = { context: "muted", container: true, title: 'Muted' };
+const containerExample = generateExampleJSX(containerArgs);
+const containerSnippet = generateCodeSnippet(containerArgs);
 
-      <h3 className='code'>Code</h3>
-      <Codeblock scroll='text'>
-{`<Block context='muted'>
-<h4>Block</h4>
-<Grid widths='1-3'>
-  <Panel>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-    eiusmod tempor incididunt ut labore et dolore magna.
-  </Panel>
-  <Panel >
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-    eiusmod tempor incididunt ut labore et dolore magna.
-  </Panel>
-  <Panel >
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-    eiusmod tempor incididunt ut labore et dolore magna.
-  </Panel>
-</Grid>
-</Block>`}
-      </Codeblock>
-    </section>
-
-    <section>
-      <h2>Block container</h2>
-      <p>
-        To put the Block in to a container use the <code>container</code> prop.
-      </p>
-
-      <h3 className='example'>Example</h3>
-
-        <Block context='muted' container >
-        <h4>Block</h4>
-        <Grid widths='1-3'>
-          <Panel>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna.
-          </Panel>
-          <Panel>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna.
-          </Panel>
-          <Panel >
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna.
-          </Panel>
-        </Grid>
-        </Block>
-
-
-      <h3 className='code'>Code</h3>
-      <Codeblock scroll='text'>
-{`<Block context='muted' container >
-<h4>Block</h4>
-<Grid widths='1-3'>
-  <Panel>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-    eiusmod tempor incididunt ut labore et dolore magna.
-  </Panel>
-  <Panel>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-    eiusmod tempor incididunt ut labore et dolore magna.
-  </Panel>
-  <Panel >
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-    eiusmod tempor incididunt ut labore et dolore magna.
-  </Panel>
-</Grid>
-</Block>`}
-      </Codeblock>
-    </section>
-
-
-    <section>
-      <h2>Block Context</h2>
-      <p>
-        To apply different background colors, add one of the following values
-        for the <code>context</code> prop.
-      </p>
-
-      <Table>
-        <thead>
-          <tr>
-            <th>Prop</th>
-            <th>Description</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td><code>default</code></td>
-            <td>Add the default background color, usually white or similar.</td>
-          </tr>
-          <tr>
-            <td><code>muted</code></td>
-            <td>Adds a light background color.</td>
-          </tr>
-          <tr>
-            <td><code>primary</code></td>
-            <td>Adds a primary background color.</td>
-          </tr>
-          <tr>
-            <td><code>secondary</code></td>
-            <td>Adds a another background color, usually a dark one.</td>
-          </tr>
-        </tbody>
-      </Table>
-
-      <h3 className='example'>Example</h3>
-      <Block context='default' container>
-      <h4>Default</h4>
-      <Grid widths='1-3'>
-        <Panel>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna.
-        </Panel>
-        <Panel >
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna.
-        </Panel>
-        <Panel >
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna.
-        </Panel>
-      </Grid>
-      </Block>
-
-      <Block context='muted' container>
-      <h4>Muted</h4>
-      <Grid widths='1-3'>
-        <Panel>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna.
-        </Panel>
-        <Panel >
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna.
-        </Panel>
-        <Panel >
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna.
-        </Panel>
-      </Grid>
-      </Block>
-
-      <Block context='primary' container contrast >
-      <h4>Primary</h4>
-      <Grid widths='1-3'>
-        <Panel>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna.
-        </Panel>
-        <Panel >
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna.
-        </Panel>
-        <Panel >
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna.
-        </Panel>
-      </Grid>
-      </Block>
-
-      <Block context='secondary' container contrast >
-      <h4>Secondary</h4>
-      <Grid widths='1-3'>
-        <Panel>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna.
-        </Panel>
-        <Panel >
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna.
-        </Panel>
-        <Panel >
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna.
-        </Panel>
-      </Grid>
-      </Block>
-
-      <h3 className='code'>Code</h3>
-      <Codeblock scroll='text'>
-{`<Block context='default' container>
-<h4>Default</h4>
-<Grid widths='1-3'>
-  <Panel>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-    eiusmod tempor incididunt ut labore et dolore magna.
-  </Panel>
-  <Panel >
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-    eiusmod tempor incididunt ut labore et dolore magna.
-  </Panel>
-  <Panel >
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-    eiusmod tempor incididunt ut labore et dolore magna.
-  </Panel>
-</Grid>
-</Block>
-
-<Block context='muted' container>
-<h4>Muted</h4>
-<Grid widths='1-3'>
-  <Panel>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-    eiusmod tempor incididunt ut labore et dolore magna.
-  </Panel>
-  <Panel >
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-    eiusmod tempor incididunt ut labore et dolore magna.
-  </Panel>
-  <Panel >
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-    eiusmod tempor incididunt ut labore et dolore magna.
-  </Panel>
-</Grid>
-</Block>
-
-<Block context='primary' container contrast >
-<h4>Primary</h4>
-<Grid widths='1-3'>
-  <Panel>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-    eiusmod tempor incididunt ut labore et dolore magna.
-  </Panel>
-  <Panel >
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-    eiusmod tempor incididunt ut labore et dolore magna.
-  </Panel>
-  <Panel >
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-    eiusmod tempor incididunt ut labore et dolore magna.
-  </Panel>
-</Grid>
-</Block>
-
-<Block context='secondary' container contrast >
-<h4>Secondary</h4>
-<Grid widths='1-3'>
-  <Panel>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-    eiusmod tempor incididunt ut labore et dolore magna.
-  </Panel>
-  <Panel >
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-    eiusmod tempor incididunt ut labore et dolore magna.
-  </Panel>
-  <Panel >
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-    eiusmod tempor incididunt ut labore et dolore magna.
-  </Panel>
-</Grid>
-</Block>`}
-      </Codeblock>
-
-      <Note badge='NOTE'>
-        To properly display colors, borders and other elements on colored blocks, you might want to apply the contrast prop to the Block componet.
-      </Note>
-    </section>
-
-
-    <section>
-      <h2>Block large</h2>
-      <p>
-        To add the body in a container use the <code>container</code> prop.
-      </p>
-
-      <h3 className='example'>Example</h3>
-        <Block context='secondary' container contrast large >
-        <h4>Block</h4>
-        <Grid widths='1-3'>
-          <Panel>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna.
-          </Panel>
-
-          <Panel >
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna.
-          </Panel>
-          <Panel >
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna.
-          </Panel>
-        </Grid>
-        </Block>
-
-      <h3 className='code'>Code</h3>
-      <h3>Code</h3>
-      <Codeblock scroll='text'>
-{`<Block context='secondary' container contrast large >
-<h4>Block</h4>
-<Grid widths='1-3'>
-  <Panel>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-    eiusmod tempor incididunt ut labore et dolore magna.
-  </Panel>
-
-  <Panel >
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-    eiusmod tempor incididunt ut labore et dolore magna.
-  </Panel>
-  <Panel >
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-    eiusmod tempor incididunt ut labore et dolore magna.
-  </Panel>
-</Grid>
-</Block>`}
-      </Codeblock>
-    </section>
-
-    <section>
-      <h2>Block Props</h2>
-
-      <p>
-        See Base for additional utility props.
-      </p>
-
-      <Table>
-        <thead>
-          <tr>
-            <th>Prop</th>
-            <th>Type</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              context
-            </td>
-            <td>enums</td>
-          </tr>
-          <tr>
-            <td>
-              container
-            </td>
-            <td colSpan='2'>oneOf <br />
-            default, muted, primary, secondary</td>
-          </tr>
-          <tr>
-            <td>
-              contrast
-            </td>
-            <td>bool</td>
-          </tr>
-          <tr>
-            <td>
-              large
-            </td>
-            <td>bool</td>
-          </tr>
-        </tbody>
-      </Table>
-    </section>
-
+// TODO: replace the calls with maps...
+const contextExamples = [
+  { context: "default", container: true, title: 'Default' },
+  { context: "muted", container: true, title: 'Muted' },
+  { context: "primary", container: true, title: 'Primary' },
+  { context: "secondary", container: true, title: 'Secondary' },
+];
+const contextTable = (
+  <Table
+    head={['Value', 'Description']}
+    body={[{
+      Value       : "default",
+      Description : "Add the default background color, usually white or similar."
+    }, {
+      Value       : <code>muted</code>,
+      Description : "Adds a light background color."
+    }, {
+      Value       : <code>primary</code>,
+      Description : "Adds the primary background color."
+    }, {
+      Value       : <code>secondary</code>,
+      Description : "Adds the secondary background color, usually a dark one."
+    }]} />
+);
+const contextExample = (
+  <div data-markdown-omit="true">
+    {contextExamples.map((example, key) => generateExampleJSX(example, key))}
   </div>
 );
 
+const contextSnippet = contextExamples
+  .map((example) => generateCodeSnippet(example))
+  .join();
 
-export default BlockDoc;
+const largeArgs = { context: "secondary", container: true, title: 'Secondary', contrast: true, large: true };
+const largeExample = generateExampleJSX(largeArgs);
+const largeSnippet = generateCodeSnippet(largeArgs);
+
+const BlockDocs = () => (
+  <div data-markdown-omit="true">
+    <DocHeader {... { name, npmName, summary }} />
+      <DocExample jsx={mutedExample} snippet={mutedSnippet} />
+      <DocExample
+        name="Block Container"
+        description="To put the Block in to a container use the <code>container</code> prop"
+        jsx={containerExample}
+        snippet={containerSnippet}
+      />
+      <DocExample
+        name="Block Context"
+        description="To apply different background colors, add one of the following values for the <code>context</code> prop."
+        table={contextTable}
+        jsx={contextExample}
+        snippet={contextSnippet}
+      />
+        <Note badge='NOTE'>
+          To properly display colors, borders and other elements on colored blocks, you might want to apply the contrast prop to the Block componet.
+        </Note>
+      <DocExample
+        name="Block large"
+        description="To add the body in a container use the <code>container</code> prop."
+        jsx={largeExample}
+        snippet={largeSnippet}
+      />
+    <DocFooter { ...{ name, propTuples }} />
+  </div>
+);
+
+export default BlockDocs;
