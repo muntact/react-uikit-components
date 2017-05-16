328eb09da42bece1dbd28d006f905d23133a56e4
Murray Lucas <murray.lucas@orionhealth.com>
2017-05-16 20:15:45 +1200

    ARTICLE: * update article doc to use snippets * add tasks to package.json to generate docs and md * changed deps in package.json to * daps

--- packages/react-uikit-article/docs/article-doc.jsx
+++ packages/react-uikit-article/docs/article-doc.jsx
@@ -1,95 +1,59 @@
-'use strict';
-
-
 import React from 'react';
-import Article from 'react-uikit-article';
-import Codeblock from 'react-uikit-codeblock';
-import Table from 'react-uikit-table';
+import Article from '../lib/article';
+// import Table from 'react-uikit-table';
 import Note from 'react-uikit-note';
 
+import DocHeader from '../../common-helpers/lib/DocHeader';
+import DocExample from '../../common-helpers/lib/DocExample';
+import DocFooter from '../../common-helpers/lib/DocFooter';
 
-const ArticleDoc = (props) => (
-  <div>
-    <section>
-      <h1>Article</h1>
-      <p className='uk-article-lead'>
-        Create articles within your page.
-      </p>
-      <p>
-        <a href='https://github.com/otissv/react-uikit-article'>react-uikit-article</a> on github.
-      </p>
-      </section>
+const name = 'Article';
+const npmName = 'react-uikit-article';
+const summary = 'Create articles within your page.';
+const propTuples = [
+  {
+    Prop: "lead",
+    Type: "string"
+    // Description: "Highlights opening paragraph of the article."
+  },
+  {
+    Prop: "meta",
+    Type: "string"
+    // Description: "Adds meta data about the article."
+  },
+  {
+    Prop: "title",
+    Type: "oneOf: 'default' | 'muted' | 'primary' | 'secondary'"
+    // Description: "Creates an article title"
+  },
+];
 
-      <section>
-        <h2>Usage</h2>
-          <Codeblock>
-{`npm install react-uikit-article --save;
+const generateExampleJSX = () => (
+  <Article
+    title='Article title'
+    meta='Written by Super User on 12 April 2012. Posted in Blog'
+    lead='Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna aliqua.' >
 
-// ES6
-import Article from 'react-uikit-article';
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    </p>
 
-// ES5
-var Article = require('react-uikit-article').default;
-`}
-        </Codeblock>
+    <hr className="uk-article-divider" />
 
-      <hr className="uk-article-divider" />
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    </p>
+    <a href="#">Read more</a>
+  </Article>
+);
 
-      <p>
-        The article component constists of the article itself, a title, a lead and meta data.
-      </p>
-
-      <Table>
-        <thead>
-          <tr>
-            <th className='uk-text-left'>Prop</th>
-            <th className='uk-text-left'>Description</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className='uk-text-left'><code>title</code></td>
-            <td className='uk-text-left'>Creates an article title.</td>
-          </tr>
-          <tr>
-            <td className='uk-text-left'><code>meta</code></td>
-            <td className='uk-text-left'>Adds meta data about the article.</td>
-          </tr>
-          <tr>
-            <td className='uk-text-left'><code>lead</code></td>
-            <td className='uk-text-left'>Highlights opening paragraph of the article</td>
-          </tr>
-        </tbody>
-      </Table>
-
-      <h3 className='example'>Example</h3>
-
-      <Article
-        title='Article title'
-        meta='Written by Super User on 12 April 2012. Posted in Blog'
-        lead='Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.' >
-
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </p>
-
-        <hr className="uk-article-divider" />
-
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </p>
-        <a href="#">Read more</a>
-      </Article>
-
-      <h4 className='code'>Code</h4>
-      <Codeblock scroll='text' syntax='xml'>
-{
-`  <Article
+const generateCodeSnippet = () =>
+`<Article
   title='Article title'
   meta='Written by Super User on 12 April 2012. Posted in Blog'
   lead='Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
@@ -109,58 +73,48 @@ var Article = require('react-uikit-article').default;
     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
   </p>
   <a href="#">Read more</a>
-</Article>
-`}
-      </Codeblock>
+</Article>`;
 
-      <Note>
-        To create separate sections in an article use <code>&lt;hr className="uk-article-divider" &#47;&gt;</code>.
-      </Note>
-    </section>
+const ArticleDoc = (props) => (
+  <div data-markdown-omit-wrapper="true">
+    <DocHeader {... { name, npmName, summary }} />
+    <p>
+      The article component constists of the article itself, a title, a lead and meta data.
+    </p>
+    {/* <Table>
+      <thead>
+        <tr>
+          <th className='uk-text-left'>Prop</th>
+          <th className='uk-text-left'>Description</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td className='uk-text-left'><code>title</code></td>
+          <td className='uk-text-left'>Creates an article title.</td>
+        </tr>
+        <tr>
+          <td className='uk-text-left'><code>meta</code></td>
+          <td className='uk-text-left'>Adds meta data about the article.</td>
+        </tr>
+        <tr>
+          <td className='uk-text-left'><code>lead</code></td>
+          <td className='uk-text-left'>Highlights opening paragraph of the article</td>
+        </tr>
+      </tbody>
+    </Table> */}
 
+    <DocExample
+      name="Example"
+      description="The article component constists of the article itself, a title, a lead and meta data"
+      jsx={generateExampleJSX()}
+      snippet={generateCodeSnippet()}
+    />
+    <Note>
+      To create separate sections in an article use <code>&lt;hr className="uk-article-divider" &#47;&gt;</code>.
+    </Note>
 
-    <section>
-      <h2>Artilce Props</h2>
-
-      <p>
-        See Base for additional utility props.
-      </p>
-
-      <Table>
-        <thead>
-          <tr>
-            <th className='uk-text-left'>Prop</th>
-            <th className='uk-text-left'>Type</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className='uk-text-left'>
-              lead
-            </td>
-            <td className='uk-text-left'>string</td>
-          </tr>
-          <tr>
-            <td className='uk-text-left'>
-              meta
-            </td>
-            <td className='uk-text-left'>string</td>
-          </tr>
-          <tr>
-            <td className='uk-text-left'>
-              onClose
-            </td>
-            <td className='uk-text-left'>func</td>
-          </tr>
-          <tr>
-            <td className='uk-text-left'>
-              title
-            </td>
-            <td className='uk-text-left'>string</td>
-          </tr>
-        </tbody>
-      </Table>
-    </section>
+    <DocFooter { ...{ name, propTuples }} />
   </div>
 );
 
