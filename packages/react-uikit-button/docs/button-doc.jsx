38a8229789b5dad27851ee3bb7814151a00de90e
Murray Lucas <murray.lucas@orionhealth.com>
2017-05-18 17:46:17 +1200

    BUTTON: update button docs, update package.json

--- packages/react-uikit-button/docs/button-doc.jsx
+++ packages/react-uikit-button/docs/button-doc.jsx
@@ -1,257 +1,150 @@
-'use strict';
-
-
-import React from 'react';
-import Button from 'react-uikit-button';
-import Codeblock from 'react-uikit-codeblock';
+import React, { PropTypes } from 'react';
+import Button from '../lib/button';
 import Table from 'react-uikit-table';
 
+import DocHeader from '../../common-helpers/lib/DocHeader';
+import DocExample from '../../common-helpers/lib/DocExample';
+import DocFooter from '../../common-helpers/lib/DocFooter';
 
-const ButtonDoc = (props) => {
-  return <div>
-    <section>
-      <h1>Button</h1>
-      <p>
-        Easily create nice looking buttons, which come in different styles.
-      </p>
-      <p>
-        <a href='https://github.com/otissv/react-uikit-button'>react-uikit-button</a> on github.
-      </p>
-    </section>
+const name = 'Button';
+const npmName = 'react-uikit-button';
+const summary = 'Easily create nice looking buttons, which come in different styles.';
 
-    <section>
-      <h2>Usage</h2>
-      <Codeblock>
-{`npm install react-uikit-button --save;
+const generateExampleJSX = ({ body, block, context, disabled, margin, type }) =>
+  <Button {...{ body, block, context, disabled, margin, type }} />;
 
-// ES6
-<code>import Button from 'react-uikit-button';
-
-// ES5
-var Button = require('react-uikit-button').default;`}
-      </Codeblock>
-
-
-      <hr/>
-
-      <p>
-        The Button component creates either a button,
-        link, submit or a close button by setting the <code>type</code>.
-      </p>
-
-      <p>
-        By defualt the Button component will create a button if no type is provided.
-      </p>
-
-      <h3 className='example'>Example</h3>
-      <Button body='Button' margin='bottom right'/>
-      <Button body='Link' type='link' margin='bottom right'/>
-      <Button body='Submit' type='submit' margin='bottom right'/>
-      <Button body='Disabled' disabled margin='bottom right'/>
-      <Button type='close'/>
-
-      <h3 className='code'>Code</h3>
-      <Codeblock>
-{`<Button body='Link' type='link' margin='bottom right'/>
-<Button body='Button' margin='bottom right'/>
-<Button body='Disabled' disabled margin='bottom right'/>
-<Button type='close'/>
-`}
-      </Codeblock>
-    </section>
-
-
-    <section>
-      <h2>Button contexts</h2>
-      <p>
-        The color of the button can be easily changed by adding a context to the context prop
-      </p>
-
-      <Table>
-        <thead>
-          <tr>
-            <th>Prop</th>
-            <th>Description</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <code>context='primary'</code>
-            </td>
-            <td>
-              Emphasizes to identify the primary action in a set of buttons.
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <code>context='success'</code>
-            </td>
-            <td>
-              Indicates a successful or positive action.
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <code>context='danger'</code>
-            </td>
-            <td>
-                Indicates a dangerous or negative action.
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <code>context='link'</code>
-            </td>
-            <td>
-              De-emphasizes to look like a link while maintaining button behavior.
-            </td>
-          </tr>
-        </tbody>
-      </Table>
-
-      <h3 className='example'>Example</h3>
-      <Button context='primary' body='Primary' margin='bottom right'/>
-      <Button context='success' body='Success' margin='bottom right'/>
-      <Button context='danger' body='Danger' margin='bottom right'/>
-      <Button context='link' body='Link' type='link'/>
-
-
-      <h3 className='code'>Code</h3>
-      <Codeblock>
-{`<Button context='primary' body='Primary' margin='bottom right'/>
-<Button context='success' body='Success' margin='bottom right'/>
-<Button context='danger' body='Danger' margin='bottom right'/>
-<Button context='link' body='Link' type='link'/>
-`}
-      </Codeblock>
-    </section>
-
-    <section>
-      <h2>Button sizes</h2>
-      <p>
-        To change the size of a Button set the <code>size</code> prop
-        to mini, small, or large.
-      </p>
-
-      <h3 className='example'>Example</h3>
-      <Button size='mini' body='Mini button' margin='right bottom'/>
-      <Button size='mini' body='Mini button' context='primary' margin='bottom'/><br />
-
-      <Button size='small' body='Small button' margin='right bottom'/>
-      <Button size='small' body='Small button' context='primary' margin='bottom'/><br />
-
-      <Button body='Default button' margin='right bottom'/>
-      <Button body='Default button' context='primary' margin='bottom'/><br />
-
-      <Button size='large' body='Large button' margin='right bottom'/>
-      <Button size='large' body='Large button' context='primary' margin='bottom'/><br />
-
-
-      <h3 className='code'>Code</h3>
-      <Codeblock>
-{`<Button size='mini' body='Mini button' margin='right bottom'/>
-<Button size='mini' body='Mini button' context='primary' margin='bottom'/><br />
-
-<Button size='small' body='Small button' margin='right bottom'/>
-<Button size='small' body='Small button' context='primary' margin='bottom'/><br />
-
-<Button body='Default button' margin='right bottom'/>
-<Button body='Default button' context='primary' margin='bottom'/><br />
-
-<Button size='large' body='Large button' margin='right bottom'/>
-<Button size='large' body='Large button' context='primary' margin='bottom'/><br />
-`}
-      </Codeblock>
-    </section>
-
-
-    <section>
-      <h2>Button blocks</h2>
-      <p>
-        To create a full width button add the <code>block</code> prop
-        to the buttton component.
-      </p>
-
-      <h3 className='example'>Example</h3>
-      <Button block body='Button' margin='smallBottom' />
-      <Button block body='Button' context='primary'/>
-
-      <h3 className='code'>Code</h3>
-      <Codeblock>
-{`
-<Button block body='Button' margin='smallBottom' />
-<Button block body='Button' context='primary'/>
-`}
-      </Codeblock>
-    </section>
-
-
-    <section>
-      <h2>Button dropdown</h2>
-      <p>
-        Buttons can also be used to trigger a dropdown.
-        See button dropdown in Dropdown component for more details.
-      </p>
-
-    </section>
-
-
-    <section>
-      <h2>Button Props</h2>
-      <p>
-        See Base for additional utility props.
-      </p>
-
-      <Table>
-        <thead>
-          <tr>
-            <th>Prop</th>
-            <th>Type</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              body
-            </td>
-            <td>string</td>
-          </tr>
-          <tr>
-            <td>
-              block
-            </td>
-            <td>bool</td>
-          </tr>
-          <tr>
-            <td>
-              context
-            </td>
-            <td>oneOf <br /> primary, success, danger or link</td>
-          </tr>
-          <tr>
-            <td>
-              disabled
-            </td>
-            <td>bool</td>
-          </tr>
-          <tr>
-            <td>
-              size
-            </td>
-            <td>oneOf <br /> mini small or large</td>
-          </tr>
-          <tr>
-            <td>
-              type
-            </td>
-            <td>oneOf <br /> button close or link</td>
-          </tr>
-        </tbody>
-      </Table>
-    </section>
-  </div>;
+generateExampleJSX.propTypes = {
+  body: PropTypes.string,
+  block: PropTypes.bool,
+  context: PropTypes.string,
+  disabled: PropTypes.bool,
+  margin: PropTypes.string,
+  type: PropTypes.string,
 };
 
+const generateCodeSnippet = ({ body, block, context, disabled, margin, size, type }) =>
+  `<Button${body ? `body="${body}"`: ""}${context ? `context="${context}"`: ""}${margin ? `margin="${margin}"`: ""}${size ? `size="${size}"`: ""}${type ? `type="${type}"`: ""}${disabled ? " disabled": ""}${block ? " block": ""} />`;
+
+// An abstraction to save the same snippet being written multiple times
+const exampleAbstraction = (argsArray) =>
+  // There is a space at the end of the wrapping span element to span the badges.
+  <div>{argsArray.map((arg) => <div>{generateExampleJSX(arg)}</div>)}</div>;
+
+const basicArgs = [
+  { body: 'Button', margin: 'bottom right' },
+  { body: 'Link', margin: 'bottom right', type: 'link' },
+  { body: 'Submit', margin: 'bottom right', type: 'submit' },
+  { body: 'Disabled', margin: 'bottom right', disabled: true },
+  { type: 'close' },
+];
+const basicExample = exampleAbstraction(basicArgs);
+const basicSnippet = basicArgs.map((arg) => generateCodeSnippet(arg)).join('\n');
+
+const contextArgs = [
+  { body: 'Primary', context: 'primary', margin: 'bottom right' },
+  { body: 'Success', context: 'success', margin: 'bottom right' },
+  { body: 'Danger', context: 'danger', margin: 'bottom right' },
+  { body: 'Link', context: 'link', type: 'Link' },
+];
+const contextExample = exampleAbstraction(contextArgs);
+const contextSnippet = contextArgs.map((arg) => generateCodeSnippet(arg)).join('\n');
+const contextTable = (
+  <Table head={['Prop', 'Description']} body={[
+    {
+      type: <code>context='primary'</code>,
+      description: 'Emphasizes to identify the primary action in a set of buttons.'
+    }, {
+      type: <code>context='success'</code>,
+      description: 'Indicates a successful or positive action.'
+    }, {
+      type: <code>context='danger'</code>,
+      description: 'Indicates a dangerous or negative action.'
+    }, {
+      type: <code>context='link'</code>,
+      description: 'De-emphasizes to look like a link while maintaining button behavior.'
+    }
+  ]} />
+);
+
+const sizeArgs = [
+  { body: 'Mini Button', size: 'mini', margin: 'bottom right' },
+  { body: 'Mini Button', size: 'mini', margin: 'bottom', context: 'primary' },
+  { body: 'Small Button', size: 'small', margin: 'bottom right' },
+  { body: 'Small Button', size: 'small', margin: 'bottom', context: 'primary' },
+  { body: 'Default Button', margin: 'bottom right' },
+  { body: 'Default Button', margin: 'bottom', context: 'primary' },
+  { body: 'Large Button', size: 'large', margin: 'bottom right' },
+  { body: 'Large Button', size: 'large', margin: 'bottom', context: 'primary' },
+];
+const sizeExample = exampleAbstraction(sizeArgs);
+const sizeSnippet = sizeArgs.map((arg) => generateCodeSnippet(arg)).join('\n');
+
+const blockArgs = [
+  { body: 'Button', margin: 'small bottom', block: true },
+  { body: 'Button', context: 'primary', block: true },
+];
+const blockExample = exampleAbstraction(blockArgs);
+const blockSnippet = blockArgs.map((arg) => generateCodeSnippet(arg)).join('\n');
+
+
+const propTuples = [{
+    prop: 'body',
+    type: 'string'
+  }, {
+    prop: 'block',
+    type: 'bool'
+  }, {
+    prop: 'context',
+    type: 'oneOf active, link, or disabled'
+  }, {
+    prop: 'disabled',
+    type: 'bool'
+  }, {
+    prop: 'size',
+    type: 'oneOf mini, small, or large'
+  }, {
+    prop: 'type',
+    type: 'oneOf button, close, or link'
+  }
+];
+
+const ButtonDoc = (props) => (
+  <div data-markdown-omit-wrapper="true">
+    <DocHeader {... { name, npmName, summary }} />
+    <DocExample
+      name="Types"
+      description={
+        `The Button component creates either a button, link, submit or a close button by setting the <code>type</code>.` +
+        `By defualt the Button component will create a button if no type is provided.`
+      }
+      jsx={basicExample}
+      snippet={basicSnippet}
+    />
+    <DocExample
+      name="Button Contexts"
+      description={`The color of the button can be easily changed by adding a context to the context prop.`}
+      jsx={contextExample}
+      table={contextTable}
+      snippet={contextSnippet}
+    />
+    <DocExample
+      name="Button Sizes"
+      description={`To change the size of a Button set the <code>size</code> prop to mini, small, or large.`}
+      jsx={sizeExample}
+      snippet={sizeSnippet}
+    />
+    <DocExample
+      name="Button Blocks"
+      description={`To create a full width button add the <code>block</code> prop to the buttton component.`}
+      jsx={blockExample}
+      snippet={blockSnippet}
+    />
+    <DocExample
+      name="Button dropdown"
+      description={`Buttons can also be used to trigger a dropdown. See button dropdown in Dropdown component for more details.`}
+    />
+    <DocFooter {...{ name, propTuples }} />
+  </div>
+);
 
 export default ButtonDoc;
