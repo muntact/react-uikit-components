0250255361a53bd6bb53d2ee738ec481a84cc6ca
Murray Lucas <murray.lucas@orionhealth.com>
2017-05-18 17:59:42 +1200

    BUTTON GROUP: add docs & package.json changes

--- packages/react-uikit-button-group/docs/button-group-doc.jsx
+++ packages/react-uikit-button-group/docs/button-group-doc.jsx
@@ -1,71 +1,57 @@
-'use strict';
-
-
 import React from 'react';
-import Button from '../../react-uikit-button/lib/button';
-import ButtonGroup from '../../react-uikit-button-group/lib/button-group';
-import Codeblock from '../../react-uikit-codeblock/lib/codeblock';
-import Table from '../../react-uikit-table/lib/table';
-import Note from '../../react-uikit-note/lib/note';
+import Button from 'react-uikit-button';
+import ButtonGroup from '../lib/button-group';
+
+import DocHeader from '../../common-helpers/lib/DocHeader';
+import DocExample from '../../common-helpers/lib/DocExample';
+import DocFooter from '../../common-helpers/lib/DocFooter';
+
+const name = 'Button Group';
+const npmName = 'react-uikit-button-group';
+const summary = 'Easily create nicely looking group buttons.';
+
+
+const generateExampleJSX = (context) => (
+  <ButtonGroup display='block' margin='smallBottom'>
+    <Button body='Link' type='link' context={context} />
+    <Button body='Button' context={context}/>
+    <Button body='Button' context={context}/>
+  </ButtonGroup>
+);
+
+const generateCodeSnippet = (context) =>
+  `<ButtonGroup display='block' margin='smallBottom'>
+  <Button body='Link' type='link'${context ? ` context="${context}"`: ""}/>
+  <Button body='Button${context ? ` context="${context}"`: ""}'/>
+  <Button body='Button${context ? ` context="${context}"`: ""}'/>
+</ButtonGroup>`;
+
+const contextArgs = [null, 'primary', 'success', 'danger'];
+const contextExample = generateExampleJSX(contextArgs);
+const contextSnippet = contextArgs.map((arg) => generateCodeSnippet(arg)).join('\n');
 
 const ButtonGroupDoc = (props) => (
-  <div>
-    <section>
-      <h1>React UIkit Button Group</h1>
-      <p>
-        Easily create nicely looking group buttons.
-      </p>
-      <p>
-        <a href='http://otissv.github.io/react-uikit-components/'>http://otissv.github.io/react-uikit-components</a> for docs.
-      </p>
-    </section>
-
-    <section>
-      <h2>Usage</h2>
-        <Codeblock>
-  {`npm install react-uikit-button-group --save;
-
-  // ES6
-  import ButtonGroup from 'react-uikit-button-group';
-
-  // ES5
-  var ButtonGroup = require('react-uikit-button-group');`}
-        </Codeblock>
-    </section>
-
-
-    <section>
-        <h2>Tests</h2>
-        <p>
-          <code>npm run test</code> to run tests with minimal output.<br />
-          <code>npm run test:spec</code> to run tests with detailed output.<br />
-          <code>npm run test:watch</code>watches all directories and run tests with minimal output on file changes.<br />
-        </p>
-      </section>
-
-      <section>
-        <h2>Build</h2>
-        <p>
-          <code>npm run build</code> to build files fro distribution.<br />
-          <code>npm run build:watch</code> watches src directory and builds files on changes.<br />
-        </p>
-      </section>
-
-
-      <section>
-        <h2>Lint</h2>
-        <p>
-          <code>npm run lint</code> lints scripts in src directory.<br />
-          <code>npm run lint:watch</code> watches src directory and lints scripts in src directory.<br />
-        </p>
-      </section>
-
-
-      <section>
-        <h2>License</h2>
-          <p>MIT</p>
-      </section>
-
+  <div data-markdown-omit-wrapper="true">
+    <DocHeader {... { name, npmName, summary }} />
+    <DocExample
+      name="Button Groups"
+      description={`To create a Button group place the Buttons inside a ButtonGroup component.`}
+      jsx={contextExample}
+      snippet={contextSnippet}
+    />
+    <DocExample
+      name="Button dropdown"
+      description={`Buttons groups can also be used to create split buttons where one button handles the standard action while the other toggles a dropdown. See button dropdown in Dropdown component for more details.`}
+    />
+    <DocExample
+      name="Checkbox buttons"
+      description={`Coming soon...`}
+    />
+    <DocExample
+      name="Radio buttons"
+      description={`Coming soon...`}
+    />
+    <DocFooter name={name} />
   </div>
 );
 
